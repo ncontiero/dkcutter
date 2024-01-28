d04fb01123822710131d14f5ccab527820225e8c
dkshs <88355080+dkshs@users.noreply.github.com>
2024-01-28 19:47:25 -0300

    feat: adding custom filter `wordCount` to count words in a string

---
+++ .changeset/khaki-readers-smash.md
@@ -0,0 +1,5 @@
+---
+"dkcutter": minor
+---
+
+feat: adding custom filter `wordCount` to count words in a string
--- src/utils/renderer.ts
+++ src/utils/renderer.ts
@@ -11,3 +11,7 @@ const globals = {
 export const renderer = nunjucks.configure({ autoescape: true });
 renderer.addGlobal("dkcutter", globals);
 renderer.addGlobal("colors", colors);
+
+renderer.addFilter("wordCount", (str: string, count: string) => {
+  return count ? str.split(count).length - 1 : str.length;
+});
