bcadc2daf306fa82bf61c6177843426420b2324d
dkshs <88355080+dkshs@users.noreply.github.com>
2025-03-12 17:57:44 -0300

    fix: typo

--- apps/docs/theme/components/Steps/index.tsx
+++ apps/docs/theme/components/Steps/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css";
 import type { ReactNode } from "react";
-import { Steps as RspressSteps } from "@theme";
+import { Steps as RspressSteps } from "rspress/theme";
 
 export function Steps(props: { children: ReactNode }) {
   return <RspressSteps {...props} />;
