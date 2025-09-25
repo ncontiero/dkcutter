7cdcb1c322724d95e6a0361c3539f1bd5ad70d82
Nicolas Contiero <git@ncontiero.com>
2025-09-25 19:23:59 -0300

    fix: remove generated test template

--- packages/dkcutter/my-awesome-project/hooks/postGenProject.ts
+++
@@ -1,5 +0,0 @@
-console.log("Running postGenProject hook...");
-
-// This is a placeholder for any post-generation logic you want to implement.
-// You can add any custom logic here that you want to run after the project is generated.
-// For example, you might want to run a script, modify files, or perform any other setup tasks.
--- packages/dkcutter/my-awesome-project/hooks/preGenProject.ts
+++
@@ -1,5 +0,0 @@
-console.log("Running preGenProject hook...");
-
-// This is a placeholder for any pre-generation logic you want to implement.
-// You can add any custom logic here that you want to run before the project is generated.
-// For example, you might want to check for required dependencies, validate inputs, or perform any other setup tasks.
