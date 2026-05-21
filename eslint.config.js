05e46e0e32633469751ea3cf32de7c7c584d8a57
Nicolas Contiero <git@ncontiero.com>
2026-05-20 23:47:16 -0300

    chore: streamline build configuration and test fixtures setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,3 @@
 import { ncontiero } from "@ncontiero/eslint-config";
 
-export default ncontiero(
-  {
-    ignores: ["./packages", "./apps"],
-  },
-  {
-    files: ["./template/hooks/**/*.ts"],
-    rules: {
-      "no-console": "off",
-    },
-  },
-);
+export default ncontiero({ ignores: ["./packages", "./apps"] });
